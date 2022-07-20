4463b42da5decb86eb5e34ff9bf7a8840aa2cb8a
rg663 <74742605+rg663@users.noreply.github.com>
2022-07-20 11:03:37 -0400

    add jquery ui, update jquery, change date format

--- coldfront/config/base.py
+++ coldfront/config/base.py
@@ -27,7 +27,8 @@ if len(SECRET_KEY) == 0:
 LANGUAGE_CODE = ENV.str('LANGUAGE_CODE', default='en-us')
 TIME_ZONE = ENV.str('TIME_ZONE', default='America/New_York')
 USE_I18N = True
-USE_L10N = True
+USE_L10N = False
+DATE_FORMAT = 'Y-m-d'
 USE_TZ = True
 
 #------------------------------------------------------------------------------
