3ea616ec62d82fc5cb23a6f01e1edd32465d35d7
riathetechie@gmail.com <74742605+rg663@users.noreply.github.com>
2024-01-06 15:02:58 -0500

    get works

--- coldfront/plugins/rest_api/views.py
+++ coldfront/plugins/rest_api/views.py
@@ -6,6 +6,7 @@ from rest_framework import serializers, generics
 from rest_framework.reverse import reverse
 from django.views.generic.base import TemplateView
 from rest_framework.renderers import JSONRenderer
+from rest_framework.authentication import TokenAuthentication
 from django.conf import settings
 
 from coldfront.core.resource.models import ResourceAttribute, Resource
@@ -13,19 +14,22 @@ from coldfront.core.allocation.models import AllocationUser, Allocation
 
 from coldfront.core.utils.common import import_from_settings
 from coldfront.plugins.slurm.associations import SlurmCluster
-from coldfront.plugins.slurm.utils import (SLURM_ACCOUNT_ATTRIBUTE_NAME,
-                                           SLURM_CLUSTER_ATTRIBUTE_NAME,
-                                           SLURM_USER_SPECS_ATTRIBUTE_NAME,
-                                           SlurmError, slurm_remove_qos,
-                                           slurm_dump_cluster, slurm_remove_account,
-                                           slurm_remove_assoc)
-
+from coldfront.plugins.slurm.utils import (
+    SLURM_ACCOUNT_ATTRIBUTE_NAME,
+    SLURM_CLUSTER_ATTRIBUTE_NAME,
+    SLURM_USER_SPECS_ATTRIBUTE_NAME,
+    SlurmError,
+    slurm_remove_qos,
+    slurm_dump_cluster,
+    slurm_remove_account,
+    slurm_remove_assoc,
+)
 
 
 from django.contrib.auth.models import User
 
 # from coldfront.icm.account_applications.models import AccountApplication, AccountApplicationsGIDChoice, AccountApplicationsStatusChoice
-from django.shortcuts import get_object_or_404#, render
+from django.shortcuts import get_object_or_404  # , render
 
 
 from django_auth_ldap.backend import LDAPBackend
@@ -38,71 +42,85 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from rest_framework import permissions
 
+
 class AllocationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Allocation
-        fields = ['id', 'all_public_attributes_as_list', 'project_id', 'resource']
+        fields = ["id", "all_public_attributes_as_list", "project_id", "resource"]
 
     resource = serializers.SerializerMethodField()
 
     def get_resource(self, obj):
         return obj.resources.first().name
 
+
 class ProjectSerializer(serializers.ModelSerializer):
     class Meta:
         model = Project
-        fields = ['id', 'title']
+        fields =  "__all__"
+
 
 class SLURMAccountsAPI(APIView):
     def get(self, request, cluster):
-        #resource = ResourceAttribute.objects.get(resource_attribute_type_id=15, value=cluster).resource
-        resource = get_object_or_404(Resource,name=cluster)
-        allocations= Allocation.objects.filter(
-                resources=resource.pk, 
-                status__name='Active', 
-                allocationuser__user__pk=request.user.pk
+        # resource = ResourceAttribute.objects.get(resource_attribute_type_id=15, value=cluster).resource
+        resource = get_object_or_404(Resource, name=cluster)
+        allocations = Allocation.objects.filter(
+            resources=resource.pk,
+            status__name="Active",
+            allocationuser__user__pk=request.user.pk,
         )
         return Response(AllocationSerializer(allocations, many=True).data)
 
+
 class SLURMAccountsPublicAPI(APIView):
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly] #this is public api
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]  # this is public api
 
     def get(self, request, username):
-        #resource = ResourceAttribute.objects.get(resource_attribute_type_id=15, value=cluster).resource
+        # resource = ResourceAttribute.objects.get(resource_attribute_type_id=15, value=cluster).resource
 
         allocations = Allocation.objects.filter(
-                resources__in=Resource.objects.filter(is_public=True, resource_type__name__in=['Cluster','Cluster Partition']), 
-                status__name='Active', 
-                allocationuser__user__username=username
+            resources__in=Resource.objects.filter(
+                is_public=True, resource_type__name__in=["Cluster", "Cluster Partition"]
+            ),
+            status__name="Active",
+            allocationuser__user__username=username,
         ).distinct()
         return Response(AllocationSerializer(allocations, many=True).data)
 
+
 class SLURMAccountsPublicAPIByAllocationStatus(APIView):
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly] #this is public api
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]  # this is public api
 
     def get(self, request, username, status):
-        #resource = ResourceAttribute.objects.get(resource_attribute_type_id=15, value=cluster).resource
+        # resource = ResourceAttribute.objects.get(resource_attribute_type_id=15, value=cluster).resource
 
         allocations = Allocation.objects.filter(
-                resources__in=Resource.objects.filter(is_public=True, resource_type__name__in=['Cluster','Cluster Partition']), 
-                status__name=status, 
-                allocationuser__user__username=username
+            resources__in=Resource.objects.filter(
+                is_public=True, resource_type__name__in=["Cluster", "Cluster Partition"]
+            ),
+            status__name=status,
+            allocationuser__user__username=username,
         ).distinct()
         print(request.user)
         return Response(AllocationSerializer(allocations, many=True).data)
 
+
 class ProjectAPI(generics.RetrieveUpdateDestroyAPIView):
+    # authentication_classes = [TokenAuthentication]
     permission_classes = [permissions.IsAuthenticated]
     serializer_class = ProjectSerializer
+    lookup_field = 'id'
 
     def get_queryset(self):
         project = Project.objects.filter(
-                id=self.kwargs.get('id'), 
+            id=self.kwargs.get("id"),
         ).distinct()
         return project
 
     def retrieve(self, request, *args, **kwargs):
-        object = Project.objects.filter(id=kwargs['id'],projectuser__user__username=request.user.username).distinct()
+        object = Project.objects.filter(
+            id=kwargs["id"], projectuser__user__username=request.user.username
+        ).distinct()
         if object:
             serializer = ProjectSerializer(object, many=True)
             response = Response(serializer.data)
@@ -111,13 +129,14 @@ class ProjectAPI(generics.RetrieveUpdateDestroyAPIView):
         else:
             response = Response()
         return response
+    
+    def update(self, request, *args, **kwargs):
+        return Response()
 
 @login_required
 def show_auth_code(request):
-
     context = {
-        'code': request.GET.get('code'),
+        "code": request.GET.get("code"),
     }
 
     return render(request, "show_code.html", context)
-
